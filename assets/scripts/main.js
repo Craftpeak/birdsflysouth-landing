444ee6e8a61b0a8c4b4693136720cdec069c3d36
Julien Melissas <julien.melissas@gmail.com>
2016-08-27 15:24:14 -0400

    Global site style stuff, using webfontloader, variables, etc

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,16 @@
-$(document).ready(function() { });
+/**
+ * WebFonts
+ */
+ WebFont.load({
+   google: {
+     families: ['Open Sans Condensed:700']
+   },
+   typekit: {
+     id: 'vbu2xyh'
+   }
+ });
+
+/**
+ * Do stuff once the page has loaded!
+ */
+$(document).ready(function() {});
